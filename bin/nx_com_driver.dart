258d928f3cd6361b21ce3295113bb23f866ab63a
NemesIX Srl <nemesixsrl@MacBook-Air-di-NemesIX.local>
2023-04-12 15:15:42 +0200

    Created the class for the driver object to communicate with the serialport

--- bin/nx_com_driver.dart
+++ bin/nx_com_driver.dart
@@ -1,5 +1,3 @@
 import 'package:nx_com_driver/nx_com_driver.dart' as nx_com_driver;
 
-void main(List<String> arguments) {
-  print('Hello world: ${nx_com_driver.calculate()}!');
-}
+void main(List<String> arguments) {}
--- lib/nx_com_driver.dart
+++ lib/nx_com_driver.dart
@@ -1,19 +1,21 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:libserialport/libserialport.dart';
 
 class NXComDriver {
   /// Serial port name
-  String _port;
+  final String _port;
 
   /// Baud rate
-  int _baudRate;
+  final int _baudRate;
 
   /// Timeout in milliseconds
-  int _timeout;
+  final int _timeout;
 
   /// Serial port object
-  late SerialPort _serialPort;
+  final SerialPort _serialPort;
 
   /// Serial port reader object
   late SerialPortReader _serialPortReader;
@@ -29,4 +31,158 @@ class NXComDriver {
         _baudRate = baudRate,
         _timeout = timeout,
         _serialPort = SerialPort(port);
+
+  /// Open serial port
+  /// It returns:
+  /// - 1: success
+  /// - -1: error
+  int open() {
+    // Check if serial port is already open
+    if (_serialPort.isOpen) {
+      try {
+        _serialPort.close();
+      } catch (_) {}
+    }
+
+    if (!_serialPort.isOpen) {
+      if (_serialPort.openReadWrite()) {
+        return -1;
+      }
+    }
+
+    // Return success
+    return 1;
+  }
+
+  /// Configure serial port
+  ///
+  /// It takes:
+  /// - stopBits: stop bits
+  /// - dataBits: data bits
+  /// - parity: parity
+  ///
+  /// It returns:
+  /// - 1: success
+  /// - -1: error
+  int config({int stopBits = 1, int dataBits = 8, int parity = 0}) {
+    try {
+      // Obtain serial port configuration
+      SerialPortConfig cfg = _serialPort.config;
+      // Set serial port configuration, baud rate
+      cfg.baudRate = _baudRate;
+      // Set serial port configuration, stop bits
+      cfg.stopBits = stopBits;
+      // Set serial port configuration, data bits
+      cfg.bits = dataBits;
+      // Set serial port configuration, parity
+      cfg.parity = parity;
+
+      // Set serial port configuration
+      _serialPort.config = cfg;
+
+      // Return success
+      return 1;
+    } catch (e) {
+      // Print error
+      print(e);
+      // Return error
+      return -1;
+    }
+  }
+
+  /// Write data to serial port
+  ///
+  /// It takes:
+  /// - data: data to write
+  ///
+  /// It returns:
+  /// - 1: success
+  /// - -1: error
+  int write(String data) {
+    try {
+      // Convert data to bytes
+      var bitData = utf8.encoder.convert(data);
+
+      // Write data to serial port
+      _serialPort.write(bitData);
+
+      // Return success
+      return 1;
+    } catch (e) {
+      // Print error
+      print(e);
+      // Return error
+      return -1;
+    }
+  }
+
+  /// Initialize serial port reader
+  ///
+  /// It returns:
+  /// - 1: success
+  /// - -1: error
+  int readerInit() {
+    try {
+      // Initialize serial port reader
+      _serialPortReader = SerialPortReader(_serialPort);
+
+      // Return success
+      return 1;
+    } catch (e) {
+      // Print error
+      print(e);
+      // Return error
+      return -1;
+    }
+  }
+
+  /// Read data from serial port
+  /// It returns:
+  /// - data: data read
+  /// - -1: error
+  Future<String> read() async {
+    // Serial port response
+    String response = '';
+
+    var completer = Completer<String>();
+
+    // Read data from serial port
+    try {
+      _serialPortReader.stream.listen((data) async {
+        response = utf8.decode(data);
+
+        completer.complete(response);
+        completer.future;
+      });
+
+      // Wait for the delay
+      await Future.delayed(Duration(milliseconds: _timeout), () {
+        // Close serial port
+        _serialPort.close();
+        // Return response
+        return response;
+      });
+
+      // Return response
+      return response;
+    } catch (e) {
+      // Print error
+      print(e);
+      // Return error
+      return '-1';
+    }
+  }
+
+  /// Info to display all the the parameters of the serial port
+  void info() {
+    // Obtain serial port configuration
+    SerialPortConfig cfg = _serialPort.config;
+
+    // Print serial port configuration
+    print('Serial port: \t\t$_port');
+    print('Baud rate: \t\t${cfg.baudRate}');
+    print('Stop bits: \t\t${cfg.stopBits}');
+    print('Data bits: \t\t${cfg.bits}');
+    print('Parity: \t\t${cfg.parity}');
+  }
 }
